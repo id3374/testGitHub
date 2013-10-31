1638ca347a052fb273532937199557ba6af79b1c
quentin <quentincarpentier@orange.fr>
2013-10-31 17:26:41 +0100

    added a simple change in the output.

--- TestCommit/src/Main.java
+++ TestCommit/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("IT WORKS !!");
+		System.out.println("IT WORKS !! test1");
 	}
 
 }
